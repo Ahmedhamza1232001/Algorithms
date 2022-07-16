cc9391a160d040687a34b83d7c062131ce19bb3f
Corleone <86518389+Ahmedhamza1232001@users.noreply.github.com>
2022-07-16 16:47:48 +0200

    Add files via upload

---
+++ BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿namespace Algorithms
+{
+    public class BinarySearch
+    {
+        
+        public static int Search(int[] myArray, int myKey)
+        {
+            int Low = 0;
+            int High = myArray.Length - 1;
+            int Mid; //what lenght do we need
+
+            while (Low < High)
+            {
+                Mid = (Low + High) / 2;
+
+                if (myKey == myArray[Mid]) //best case 
+                    return Mid;
+
+                if (myKey < myArray[Mid])
+                    High = Mid - 1;
+
+                else 
+                    Low = Mid + 1;
+
+            }
+            return -1;
+
+        }
+    
+
+
+
+    }
+
+}
