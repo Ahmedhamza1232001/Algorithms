992c7cdfde6ccdfbbfc7529cf6b9c58db7cac866
Corleone <86518389+Ahmedhamza1232001@users.noreply.github.com>
2022-07-17 03:12:35 +0200

    Add files via upload

---
+++ SlectionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Algorithms
+{
+    public class SelectionSort
+    {
+        public static int[] Sort(int[]  unsortedArray)
+        {
+            
+            for (int i = 0 ; i <  unsortedArray.Length; i++)
+			{
+                int minValue = unsortedArray[i];
+                int temp = i;
+                for (int m = i+1 ; m < unsortedArray.Length-1 ; m++)
+			    {
+                
+                if (minValue> unsortedArray[m])
+                    {
+                    minValue = unsortedArray[m];
+                    temp = m;
+                    }
+	
+			    }
+
+                
+                unsortedArray[temp] = unsortedArray[i];
+                unsortedArray[i] = minValue;
+                
+			}
+            return unsortedArray;
+        }  
+
+    }
+}
