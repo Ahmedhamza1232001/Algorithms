2a840e53e383e9c88fc68413c764b8ef5780f9dc
Corleone <86518389+Ahmedhamza1232001@users.noreply.github.com>
2022-07-18 23:21:14 +0200

    Add files via upload

---
+++ RecursiveBinarySearch.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithms
+{
+    public class RecursiveBinarySearch
+
+    {
+        public static int Search(int[] sortedArray ,int low , int high, int key)
+        {
+            if (low <= high)
+            {
+                int mid = (low+high)/2;
+                if (sortedArray[mid] == key)
+                {
+                    return mid;
+                }
+                if(sortedArray[mid] < key)
+                {
+                    return Search(sortedArray, mid+1, high, key);    
+                }
+                
+                
+                return Search(sortedArray,low,mid-1,key);
+                
+                  
+            }
+            return -1;
+        }
+    }
+}
